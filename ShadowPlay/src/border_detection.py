c973baaa6eaf0496825940fecc58eb02f3e20d65
100383960 <100383960@alumnos.uc3m.es>
2023-12-17 01:11:05 +0100

    Se ingluye programa para entrenar con bordes

--- ShadowPlay/src/border_detection.py
+++ ShadowPlay/src/border_detection.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 img_path = 'test.jpeg'
 img = cv2.imread(img_path)
 
-imagen_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-imagen_gris = cv2.resize(imagen_gris, (416,416), interpolation=cv2.INTER_LINEAR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (416,416), interpolation=cv2.INTER_LINEAR)
 
-borders_canny = cv2.Canny(img, 200,220)
+img = cv2.Canny(img,100,220)
 
-print(borders_canny.shape)
+print(img.shape)
 
-cv2.imshow('Imagen en Escala de Grises', imagen_gris)
+cv2.imshow('Imagen en Escala de Grises', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
